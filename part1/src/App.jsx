9374b089aa05fcab35823b44922ec791f0ae1804
magimoose <magnus@boulevardtech.fi>
2026-07-08 21:54:47 +0300

    anecdotes

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,67 +1,46 @@
 import { useState } from 'react'
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ]
 
-const StatisticsLine = ({ text, value }) => {
-  return (
-    <>
-      <td> {text}</td>
-      <td> {value} </td>
-    </>
-  )
-}
-const Statistics = (props) => {
-  if (props.all === 0) {
-    return (
-      <div>
-        <h2>statistics</h2>
-        <div>No feedback given</div>
-      </div>
+  const addVotes = () => {
+    const copy = { ...votes }
+    copy[selected] += 1
+    setVotes(copy)
+    console.log('given one vote to ', selected)
+  }
+
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0 })
+  console.log(Object.values(votes))
+  const max = () => {
+    console.log('max called')
+    return Object.keys(votes).reduce((lead, current) =>
+      votes[current] > votes[lead] ? current : lead,
     )
   }
   return (
     <div>
-      <h2>statistics</h2>
-      <table>
-        <tr>
-          <StatisticsLine text="good" value={props.good} />
-        </tr>
-        <tr>
-          <StatisticsLine text="neutral" value={props.neutral} />
-        </tr>
-        <tr>
-          <StatisticsLine text="bad" value={props.bad} />
-        </tr>
-        <tr>
-          <StatisticsLine text="all" value={props.all} />
-        </tr>
-        <tr>
-          <StatisticsLine text="average" value={(props.good - props.bad) / props.all} />
-        </tr>
-        <tr>
-          <StatisticsLine text="positive" value={props.good / props.all} /> %
-        </tr>
-      </table>
-    </div>
-  )
-}
-
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  let all = good + neutral + bad
-
-  return (
-    <div>
-      <h2>give feedback</h2>
-      <div>
-        <Button onClick={() => setGood(good + 1)} text="good" />
-        <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
-        <Button onClick={() => setBad(bad + 1)} text="bad" />
-        <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-      </div>
+      <h2>Anecdote of the day</h2>
+      {anecdotes[selected]}
+      <br />
+      has {votes[selected]} votes
+      <br />
+      <button onClick={() => setSelected(Math.floor(Math.random() * 8))}>next anecdote</button>
+      <button onClick={addVotes}>vote</button>
+      <h2>Anecdote with the most votes</h2>
+      {anecdotes[max()]}
+      <br />
+      has {votes[max()]} votes
     </div>
   )
 }
